972594ee6fb2b5f860e0034268c26fda3d7e1c95
nipengg <nevillecorneliustj@gmail.com>
2021-11-03 16:29:15 +0700

    Update Dashboard

--- resources/js/Jetstream/Welcome.vue
+++ resources/js/Jetstream/Welcome.vue
@@ -10,10 +10,14 @@
             </div>
 
             <div class="mt-6 text-gray-500">
-                Laravel Jetstream provides a beautiful, robust starting point for your next Laravel application. Laravel is designed
-                to help you build your application using a development environment that is simple, powerful, and enjoyable. We believe
-                you should love expressing your creativity through programming, so we have spent time carefully crafting the Laravel
-                ecosystem to be a breath of fresh air. We hope you love it.
+                Laravel + Vue JS Chat App is a chat application like Discord text channel that you can use to communicate with other user,
+                You can also manage your channel room like create a new one or delete your channel. with Laravel Jetstream it provides a
+                beautiful development environment that help building user authentication scaffolding. Feel free to try the Chat App feature
+                <br>
+                <br>
+                Thank You!
+                <br>
+                Neville Cornelius Tjampan
             </div>
         </div>
     </div>
